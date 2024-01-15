a99ebc53d1c53f34767b53965b4bc2a2bb9bef5d
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2024-01-15 14:49:57 +0300

    update

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input";
-import { MatInputModule } from "@angular/material/select";
-import { MatInputModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/radio";
-import { MatInputModule } from "@angular/material/input";
-import { MatInputModule } from "@angular/material/input";
-import { MatInputModule } from "@angular/material/input";
-import { MatInputModule } from "@angular/material/input";
-import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCardModule } from "@angular/material/card";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatDialogModule } from "@angular/material/dialog";
 
 
 @NgModule({
   exports: [
-
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatSortModule
   ]
 })
 export class MaterialModule {}
