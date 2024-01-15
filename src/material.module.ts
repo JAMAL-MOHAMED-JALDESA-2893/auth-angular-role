dceaa6b6f49a5d5ffaf84844441c0f133837a3d1
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2024-01-15 14:43:40 +0300

    update

--- src/material.module.ts
+++ src/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input";
 import { MatInputModule } from "@angular/material/select";
 import { MatInputModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
+import { MatInputModule } from "@angular/material/radio";
 import { MatInputModule } from "@angular/material/input";
 import { MatInputModule } from "@angular/material/input";
 import { MatInputModule } from "@angular/material/input";
