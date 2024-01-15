c0c51a6d314074f7d13ec4d60fc862b9a4a4a3b9
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2024-01-15 16:30:43 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,17 @@ import { ToastrModule } from 'ngx-toastr';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { UserlistingComponent } from './userlisting/userlisting.component';
+import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    UserlistingComponent,
+    UpdatepopupComponent
   ],
   imports: [
     BrowserModule,
