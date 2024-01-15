1bfba930d99114d6da372c611dce922c48cb0763
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2024-01-15 14:42:29 +0300

    update

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from "@angular/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatInputModule } from "@angular/material/input";
+
 
 @NgModule({
   exports: [
